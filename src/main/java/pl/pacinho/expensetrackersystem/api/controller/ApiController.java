a0f0fb907f351146e8d693a2108599c19f0d0a5e
patryk_ojdana <patryk.ojdana@axfina.pl>
2024-03-15 07:48:43 +0100

    Add endpoint for get categories

--- src/main/java/pl/pacinho/expensetrackersystem/api/controller/ApiController.java
+++ src/main/java/pl/pacinho/expensetrackersystem/api/controller/ApiController.java
@@ -3,6 +3,7 @@ package pl.pacinho.expensetrackersystem.api.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.pacinho.expensetrackersystem.expense.model.enums.Category;
 
 @RequestMapping("/api")
 @RestController
@@ -11,5 +12,10 @@ public class ApiController {
     @GetMapping("/ping")
     String ping() {
         return "OK";
+    }
+
+    @GetMapping("/category")
+    Category[] getCategories() {
+        return Category.values();
     }
 }
